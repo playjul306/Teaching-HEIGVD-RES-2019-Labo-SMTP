cf25329e7aa395f2aade11d442810c0a52afbc10
volkan05 <volkan.sutcu@heig-vd.ch>
2019-04-03 11:27:26 +0200

    fichier prank ok

--- smtpclient/src/main/java/model/prank/Prank.java
+++ smtpclient/src/main/java/model/prank/Prank.java
@@ -1,4 +1,77 @@
 package model.prank;
 
+import model.mail.Group;
+import model.mail.Message;
+import model.mail.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Prank {
+
+    private Person sender = new Person();
+    private Group receivers = new Group();
+    private Group copyCarbon = new Group();
+    private String message;
+
+    //------------------ Setter --------------
+    public void setSender(Person sender){
+        this.sender = sender;
+    }
+
+    public void setReceivers(Group receivers){
+        this.receivers.setGroup(receivers);
+    }
+
+    public void setCopyCarbon(Group copyCarbon){
+        this.copyCarbon.setGroup(copyCarbon);
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    //------------------ Getter --------------
+    public Person getSender(){
+        return sender;
+    }
+
+    public Group getReceivers(){
+        return receivers;
+    }
+
+    public Group getCopyCarbon(){
+        return copyCarbon;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public Message generateMail(){
+        Message mail = new Message();
+
+        mail.setFrom(sender.getEmail());
+
+        // Récupère les informations pour le champ qui concerne les destinataires
+        List<String> recptTo = new ArrayList<String>();
+        List<Person> memberOfReceivers = receivers.getMember();
+        for(Person person : memberOfReceivers){
+            recptTo.add(person.getEmail());
+        }
+        mail.setRcptTo(recptTo);
+
+        // Récupère les informations pour le champ qui concerne les copies cachées
+        List<String> copyCarbon = new ArrayList<String>();
+        List<Person> memberOfCopyCarbon = copyCarbon.getMember();
+        for(Person person : memberOfCopyCarbon){
+            copyCarbon.add(person.getEmail());
+        }
+        mail.setCopyCarbon(copyCarbon);
+
+        mail.setData(message + sender.getLastName() + sender.getFirstName());
+
+        return mail;
+    }
+
 }
