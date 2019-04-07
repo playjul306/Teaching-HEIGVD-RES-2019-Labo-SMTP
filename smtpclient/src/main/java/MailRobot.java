ea8262adb427d41e09bdeb37ed5d9b7cbad35678
Julien Benoit <julienbenoit@mbp-de-julien.home>
2019-04-07 21:32:03 +0200

    MailRobot done

--- smtpclient/src/main/java/MailRobot.java
+++ smtpclient/src/main/java/MailRobot.java
@@ -1,7 +1,33 @@
+import config.*;
+import model.prank.*;
+import smtp.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.*;
+
 public class MailRobot {
+    private static final Logger LOG = Logger.getLogger(MailRobot.class.getName());
+
     public static void main(String[] args){
+        IConfigurationManager config;
+        PrankGenerator prankGenerator;
+        List<Prank> prankList;
+        ISmtpClient client;
 
+        try {
+            config = new ConfigurationManager();
+            prankGenerator = new PrankGenerator(config);
+            prankList = prankGenerator.generatePranks();
+            client = new SmtpClient(config);
 
+            for (Prank p : prankList){
+                client.sendMessage(p.generateMail());
+            }
+
+        } catch (IOException e){
+            LOG.log(Level.SEVERE, "Impossible d'envoyer le prank : {0}", e.getMessage());
+            e.printStackTrace();
+        }
         
     }
 }
