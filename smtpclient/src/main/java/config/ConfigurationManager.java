57178c3e4ab96d2de6243069e6874670b69d5f9c
volkan05 <volkan.sutcu@heig-vd.ch>
2019-04-09 19:54:27 +0200

    3 fichiers commentés

--- smtpclient/src/main/java/config/ConfigurationManager.java
+++ smtpclient/src/main/java/config/ConfigurationManager.java
@@ -13,27 +13,50 @@ public class ConfigurationManager implements IConfigurationManager {
     private List<String> messages;
     private Group victims;
 
+    /**
+     * Permet d'instancier un objet contenant les informations fournies pour le serveur via le chemin par défaut
+     * @throws IOException levée en cas d'erreur lors du chargement des fichiers
+     */
     public ConfigurationManager() throws IOException {
         this(defaultConfigPath);
     }
 
+    /**
+     * Permet d'instancier un objet contenant les informations fournies pour le serveur via un chemin
+     * @param configFilesPath chemin où se trouve les différents fichiers
+     * @throws IOException levée en cas d'erreur lors du chargement des fichiers
+     */
     public ConfigurationManager(String configFilesPath) throws IOException {
         properties = loadProperties(configFilesPath);
         messages = loadMessages(configFilesPath);
         victims = loadVictims(configFilesPath);
     }
 
+    /**
+     * Permet de charger les informations de configuration
+     * @param configFilesPath chemin où se trouve les différents fichiers
+     * @return une instance contenant les informations de configuration
+     * @throws IOException levée en cas d'erreur lors du chargement du fichier de configuration
+     */
     private Properties loadProperties(String configFilesPath) throws IOException{
         properties = new Properties();
         properties.load(new FileInputStream(configFilesPath + configFileName));
         return properties;
     }
 
+    /**
+     * Permet de charger les blagues fournies
+     * @param configFilesPath chemin où se trouve les différents fichiers
+     * @return une liste des différentes blague récupérées
+     * @throws IOException levée en cas d'erreur lors du chargement du fichier de blagues
+     */
     private List<String> loadMessages(String configFilesPath) throws IOException{
         messages = new ArrayList<String>();
         FileInputStream loadFile = new FileInputStream(configFilesPath + messagesFileName);
+        // Instancie un lecteur sur le fichier
         BufferedReader reader = new BufferedReader(new InputStreamReader(loadFile, "UTF-8"));
         String completeMessage = "", line = "";
+        // On récupère les blagues séparées par un "==" tant que la ligne lue contient quelque chose
         while((line = reader.readLine()) != null){
             if(line.equals("==")){
                 messages.add(completeMessage);
@@ -47,11 +70,18 @@ public class ConfigurationManager implements IConfigurationManager {
         return messages;
     }
 
+    /**
+     * Permet de charger les victimes des blagues
+     * @param configFilesPath chemin où se trouve les différents fichiers
+     * @return un groupe où se trouve toutes les personnes du fichier
+     * @throws IOException levée en cas d'erreur lors du chargement du fichier de victimes
+     */
     private Group loadVictims(String configFilesPath) throws IOException{
         victims = new Group();
         FileInputStream loadFile = new FileInputStream(configFilesPath + victimsFileName);
         String line = "";
         BufferedReader reader = new BufferedReader(new InputStreamReader(loadFile, "UTF-8"));
+        // Chaque ligne lu et contenant une victime est ajouté à la liste
         while((line = reader.readLine()) != null){
             victims.addPersons(new Person(line));
         }
@@ -60,31 +90,56 @@ public class ConfigurationManager implements IConfigurationManager {
         return victims;
     }
 
+    /**
+     * Récupère l'adresse du serveur smtp
+     * @return la chaîne de caractère représentant le serveur smtp
+     */
     public String getSmtpServerAdress(){
         return properties.getProperty(field_smtpServerAddress);
     }
 
+    /**
+     * Récupère le port du serveur smtp
+     * @return le numéro de port du serveur smtp
+     */
     public int getSmtpServerPort(){
         return Integer.parseInt(properties.getProperty(field_smtpServerPort));
     }
 
+    /**
+     * Récupère le nombre de groupe à former pour les blagues
+     * @return le nombre de groupe à former
+     */
     public int getNumberOfGroups(){
         return Integer.parseInt(properties.getProperty(field_numberOfGroups));
     }
 
+    /**
+     * Récupère le groupe de personne à mettre en copie cachée dans le mail
+     * @return le groupe de personnes pour la copie cachée
+     */
     public Group getWitnessesToCC(){
         String[] witnesses = properties.getProperty(field_witnessesToCC).split(",");
         Group groupOfWitnesses = new Group();
+        // Chaque personne mentionnée pour la copie cachée est ajouté dans la liste pour ce rôle
         for(String eachWitness : witnesses){
             groupOfWitnesses.addPersons(new Person(eachWitness));
         }
         return groupOfWitnesses;
     }
 
+    /**
+     * Récupère les messages sous forme d'une liste
+     * @return la liste des messages
+     */
     public List<String> getMessages(){
         return messages;
     }
 
+    /**
+     * Récupère les victimes du mail
+     * @return un groupe de personnes représentant les victimes de la blague
+     */
     public Group getVictims(){
         return victims;
     }
--- smtpclient/src/main/java/model/prank/Prank.java
+++ smtpclient/src/main/java/model/prank/Prank.java
@@ -15,45 +15,84 @@ public class Prank {
     private String message;
 
     //------------------ Setter --------------
+
+    /**
+     * Applique un envoyeur pour de la blague
+     * @param sender la personne faisant office d'envoyeur
+     */
     public void setSender(Person sender){
         this.sender = sender;
     }
 
+    /**
+     * Applique des receveurs de la blague
+     * @param receivers les personnes faisant office de receveurs
+     */
     public void setReceivers(Group receivers){
         this.receivers.setGroup(receivers);
     }
 
+    /**
+     * Applique des personnes en copies cachées
+     * @param copyCarbon les personnes mentionnées en copies cachées
+     */
     public void setCopyCarbon(Group copyCarbon){
         this.copyCarbon.setGroup(copyCarbon);
     }
 
+    /**
+     * Applique le message fourni pour le mail à construire
+     * @param message le message contenu dans le mail
+     */
     public void setMessage(String message){
         this.message = message;
     }
 
     //------------------ Getter --------------
+
+    /**
+     * Récupère l'envoyeur de la blague
+     * @return retourne la personne qui envoit le mail
+     */
     public Person getSender(){
         return sender;
     }
 
+    /**
+     * Récupère les receveurs de la blague
+     * @return le groupe de personnes recevant la blague
+     */
     public Group getReceivers(){
         return receivers;
     }
 
+    /**
+     * Récupère les personnes mentionnées en copies cachées
+     * @return le group de personnes en copies cachées
+     */
     public Group getCopyCarbon(){
         return copyCarbon;
     }
 
+    /**
+     * Récupère le message du mail
+     * @return une chaîne de caractère représentant le message dans le mail
+     */
     public String getMessage(){
         return message;
     }
 
+    /**
+     * Génère le mail complet avec toutes les informations nécessaires
+     * @return le mail construit et prêt à l'envoi
+     */
     public Message generateMail(){
         Message mail = new Message();
 
+        // L'envoyeur est récupéré et mentionné
         mail.setFrom(sender.getEmail());
 
-        // Récupère les informations pour le champ qui concerne les destinataires
+        // Les personnes allant recevoir le mail sont récupérées et ajoutées
         List<String> recptTo = new ArrayList<String>();
         List<Person> memberOfReceivers = receivers.getMember();
         for(Person person : memberOfReceivers){
@@ -61,7 +100,7 @@ public class Prank {
         }
         mail.setRcptTo(recptTo);
 
-        // Récupère les informations pour le champ qui concerne les copies cachées
+        // Les personnes en copies cachées sont parcourues et ajoutées
         List<String> cc = new ArrayList<String>();
         List<Person> memberOfCopyCarbon = copyCarbon.getMember();
         for(Person person : memberOfCopyCarbon){
@@ -69,6 +108,7 @@ public class Prank {
         }
         mail.setCopyCarbon(cc);
 
+        // En plus du message complet dans le mail, une signature est ajoutée
         mail.setData(message + "\r\n" + sender.getLastName() + " " + sender.getFirstName());
 
         return mail;
